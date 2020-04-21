ef736d16b1b29e42f0c8b947b34a347343f0dd43
IvanNik85 <ivannikolic0885@gmail.com>
2020-04-22 00:09:41 +0200

    scaling created tables on front

--- src/client/components/Pages/RestaurantFront.js
+++ src/client/components/Pages/RestaurantFront.js
@@ -1,36 +1,56 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../../context/AppContext';
 
+const useWindowSize = () => {
+    const [size, setSize] = useState([0, 0]);
+
+    useEffect(() => {
+        function updateSize() {
+            setSize([window.innerWidth, window.innerHeight]);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+    }, []);
+
+    return size;
+}
+
 const RestaurantFront = () => {
     const context = useContext(AppContext);
+    const [initialW, setInitial] = useState(window.innerWidth)
     const styleTable = context.table;
-    console.log(context.allData)
+    const [width, height] = useWindowSize();
+    let zoomPercent = width / initialW * 100;
+
     return (
         <div>
             <div className="selection">
-                <h1>Choose yor table!</h1>
+                <h1>Select time, date</h1>                
             </div>
 
-            <div className="restoraunt-area front">
-                {context.allData.map((item, i) => {
-                    let [rotValue] = item.rotated.match(/\d+/g)
-                    return (<div className="table-basic"
-                        key={i}
-                        style={
-                            {
-                                width: `${item.width}`,
-                                height: `${item.height}`,
-                                transform: `${item.cordinates} ${item.rotated}`,
-                                background: `${styleTable(item.seats.length).background}`
+            <div className="restoraunt-wrap">
+                <div className="restoraunt-area front" style={{ zoom: `${zoomPercent}%`}}>
+                    {context.allData.map((item, i) => {
+                        let [rotValue] = item.rotated.match(/\d+/g)
+                        return (<div className="table-basic"
+                            key={i}
+                            style={
+                                {
+                                    width: `${item.width}`,
+                                    height: `${item.height}`,
+                                    transform: `${item.cordinates} ${item.rotated}`,
+                                    background: `${styleTable(item.seats.length).background}`
+                                }
                             }
-                        }
-                        onClick={() => console.log(`FRONT`)}
-                    >
-                        {item.seats}
-                        <i className="fas fa-utensils" style={{transform: `rotate(-${rotValue}deg)`}}></i>
-                    </div>)
-                })
-                }
+                            onClick={() => console.log(`FRONT`)}
+                        >
+                            {item.seats}
+                            <i className="fas fa-utensils" style={{ transform: `rotate(-${rotValue}deg)` }}></i>
+                        </div>)
+                    })
+                    }
+                </div>
             </div>
         </div>
     )
--- src/server/db/mongoose.js
+++ src/server/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 // Connect to database
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .catch(er => console.log("Mongoose connection error: ", er.message));
+// mongoose
+//   .connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .catch(er => console.log("Mongoose connection error: ", er.message));
